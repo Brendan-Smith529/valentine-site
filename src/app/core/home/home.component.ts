2735e42c777eb9ca7d2ea166f22573b93e36eb32
Brendan <brendansmith5292002@gmail.com>
2025-02-13 21:34:47 -0500

    Update button styles and movement

--- src/app/core/home/home.component.ts
+++ src/app/core/home/home.component.ts
@@ -1,3 +1,4 @@
+import { CommonModule } from '@angular/common';
 import { AfterViewInit, Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
@@ -12,19 +13,29 @@ const phrases = [
 @Component({
   selector: 'app-home',
   imports: [
+    CommonModule,
     RouterLink
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent implements AfterViewInit {
+  moved: boolean = false;
   private lastNum: number = -1;
 
   ngAfterViewInit(): void {
     const button = document.getElementById("declineBtn");
     if (!button) return;
 
+    // Set button as percentage to avoid conversion later
+    const rect = button.getBoundingClientRect();
+    button.style.left = `${(rect.left / window.innerWidth) * 100}%`;
+    button.style.top = `${(rect.top / window.innerHeight) * 100}%`;
+
     button.addEventListener('mouseover', () => {
+      if (!this.moved)
+        this.moved = true;
+
       button.style.left = `${Math.ceil(Math.random() * 90)}%`;
       button.style.top = `${Math.ceil(Math.random() * 90)}%`;
     });
